830468257468583eb100d76da986b9c74353e4b6
antoniocasto <antonio9_4@outlook.com>
2019-11-19 12:58:03 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'src/articles.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Article> _articles = articles;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          //Metodo per gestire il refresh -> Qui posso scaricare dati da internet per esempio
+          await Future.delayed(const Duration(seconds: 1));
+          setState(() {
+            _articles.removeAt(0);
+          });
+        },
+        child: ListView(
+          children: _articles.map(_buildItem).toList(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildItem(Article article) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ExpansionTile(
+        title: Text(
+          article.text,
+          style: TextStyle(
+            fontSize: 24.0,
+          ),
+        ),
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text(
+                "${article.commentsCount} comments",
+              ),
+              IconButton(
+                icon: Icon(Icons.launch),
+                onPressed: () async {
+                  final fakeUrl = "http://${article.domain}";
+
+                  if (await canLaunch(fakeUrl)) {
+                    launch(fakeUrl);
+                  }
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/articles.dart
@@ -0,0 +1,91 @@
+class Article {
+  final String text;
+  final String domain;
+  final String by;
+  final String age;
+  final int score;
+  final int commentsCount;
+
+  const Article(
+      {this.text,
+      this.domain,
+      this.by,
+      this.age,
+      this.score,
+      this.commentsCount});
+}
+
+final articles = [
+  Article(
+    text: "Juventus wins the game.",
+    domain: "Juventus.com",
+    by: "jvvv",
+    age: "25",
+    score: 177,
+    commentsCount: 62,
+  ),
+  Article(
+    text: "Milan wins the game.",
+    domain: "Milan.com",
+    by: "jvvv",
+    age: "25",
+    score: 33,
+    commentsCount: 22,
+  ),
+  Article(
+    text: "Inter wins the game.",
+    domain: "Inter.com",
+    by: "jvvv",
+    age: "25",
+    score: 1,
+    commentsCount: 1,
+  ),
+  Article(
+    text: "Inter wins the game.",
+    domain: "Inter.com",
+    by: "jvvv",
+    age: "25",
+    score: 1,
+    commentsCount: 1,
+  ),
+  Article(
+    text: "Inter wins the game.",
+    domain: "Inter.com",
+    by: "jvvv",
+    age: "25",
+    score: 1,
+    commentsCount: 1,
+  ),
+  Article(
+    text: "Inter wins the game.",
+    domain: "Inter.com",
+    by: "jvvv",
+    age: "25",
+    score: 1,
+    commentsCount: 1,
+  ),
+  Article(
+    text: "Inter wins the game.",
+    domain: "Inter.com",
+    by: "jvvv",
+    age: "25",
+    score: 1,
+    commentsCount: 1,
+  ),
+  Article(
+    text: "Inter wins the game.",
+    domain: "Inter.com",
+    by: "jvvv",
+    age: "25",
+    score: 1,
+    commentsCount: 1,
+  ),
+  Article(
+    text: "Inter wins the game.",
+    domain: "Inter.com",
+    by: "jvvv",
+    age: "25",
+    score: 1,
+    commentsCount: 1,
+  ),
+];
